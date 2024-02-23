346b2bc28fb75dac1b3e87d3a99b5145e7f3bc50
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-23 03:11:38 -0800

    Seiscope add

---
+++ IntegralEquation/Scripts-Seiscope/BashScripts/p01.sh
@@ -0,0 +1,8 @@
+# Run from within this directory
+
+cd /home/rsarkar/
+source .bashrc
+conda activate py39
+cd /home/rsarkar/Research/Thesis
+
+python -m InversionLS.IntegralEquation.Scripts-Seiscope.p01_create_params_jsonfile
