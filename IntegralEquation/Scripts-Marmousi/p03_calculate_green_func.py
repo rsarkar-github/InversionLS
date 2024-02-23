7543c52c07a04ff99880aa08c4c244d392896c65
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-23 03:59:48 -0800

    Updates

--- IntegralEquation/Scripts-Marmousi/p03_calculate_green_func.py
+++ IntegralEquation/Scripts-Marmousi/p03_calculate_green_func.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     print("---------------------------------------------")
     print("Calculating Green's functions...")
     print("\n")
-    num_procs = 43
+    num_procs = 1
     obj.calculate_greens_func(num_procs=num_procs)
