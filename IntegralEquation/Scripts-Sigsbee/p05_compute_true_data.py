a54e88e5be9bed95526e5cb2485134f25be1bd10
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-23 05:23:23 -0800

    Sigsbee add

---
+++ IntegralEquation/Scripts-Sigsbee/p05_compute_true_data.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from ..Inversion.ScatteringIntegralGeneralVzInversion import ScatteringIntegralGeneralVzInversion2d
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+    basedir = "InversionLS/Expt/sigsbee/"
+    obj = ScatteringIntegralGeneralVzInversion2d(
+        basedir=basedir,
+        restart=True,
+        restart_code=None
+    )
+
+    print("\n\n---------------------------------------------")
+    print("---------------------------------------------")
+    print("Compute true data...")
+    print("\n")
+
+    num_procs = obj.num_sources
+    max_iter = 200
+    tol = 1e-5
+    verbose = False
+    obj.compute_true_data(num_procs=obj.num_sources, max_iter=max_iter, tol=tol, verbose=verbose)
