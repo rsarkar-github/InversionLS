64684918f414d65c7f251197d81122edce412727
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-23 00:42:18 -0800

    BashScripts directory add

---
+++ IntegralEquation/Scripts/BashScripts/p01a.sh
@@ -0,0 +1,8 @@
+# Run from within this directory
+
+cd /home/rsarkar/
+source .bashrc
+conda activate py39
+cd /home/rsarkar/Research/Thesis
+
+python -m InversionLS.Scripts.p01a_create_params_jsonfile.py
