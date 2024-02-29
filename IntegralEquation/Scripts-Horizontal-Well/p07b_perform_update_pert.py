9c024ddcb7aa27fd85a8cfc3038a4d332b6b6f6b
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-29 06:32:00 -0800

    Major updates to 2D inversion class

---
+++ IntegralEquation/Scripts-Horizontal-Well/p07b_perform_update_pert.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+from ..Inversion.ScatteringIntegralGeneralVzInversion import ScatteringIntegralGeneralVzInversion2d
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+    basedir = "InversionLS/Expt/horizontal-well/"
+    obj = ScatteringIntegralGeneralVzInversion2d(
+        basedir=basedir,
+        restart=True,
+        restart_code=7
+    )
+
+    print("\n\n---------------------------------------------")
+    print("---------------------------------------------")
+    print("Perform update...")
+    print("\n")
+
+    obj.perform_inversion_update_model_pert(
+        iter_count=0,
+        max_iter=50,
+        tol=1e-5,
+        num_procs=16,
+        clean=False
+    )
