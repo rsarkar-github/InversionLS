e1c9bbc3e255b99f2c6499cbdc0d5716e8368e30
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-08 13:58:54 -0800

    Updates

--- IntegralEquation/Scripts-Horizontal-Well/q04_display_iter_pert.py
+++ IntegralEquation/Scripts-Horizontal-Well/q04_display_iter_pert.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         aspect_ratio=10, aspect_cbar=10, file_name=None
     )
     plot1(
-        vel=pert, extent=extent, title="Inverted pert", vmin=-scale/5000, vmax=scale/5000,
+        vel=pert, extent=extent, title="Inverted pert", vmin=-scale/1000, vmax=scale/1000,
         aspect_ratio=10, aspect_cbar=10, file_name=None
     )
 
