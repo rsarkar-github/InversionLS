4aac672cf7745f66437c237d4af072eaec9f8b6b
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-08 01:30:13 -0800

    Debugging

--- IntegralEquation/Scripts-Horizontal-Well/p06_set_initial_pert_wavefields.py
+++ IntegralEquation/Scripts-Horizontal-Well/p06_set_initial_pert_wavefields.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from ..Inversion.ScatteringIntegralGeneralVzInversion import ScatteringIntegralGeneralVzInversion2d
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 
 if __name__ == "__main__":
@@ -19,4 +20,5 @@ if __name__ == "__main__":
     print("\n")
 
     # obj.set_zero_initial_pert_wavefields(num_procs=10)
-    obj.set_zero_initial_pert(num_procs=16)
+    num_procs = min(obj.num_sources, mp.cpu_count(), 100)
+    obj.set_zero_initial_pert(num_procs=num_procs)
