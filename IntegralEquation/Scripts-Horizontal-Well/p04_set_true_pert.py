57fd1651badd50831783ef6160774a4277a44d5c
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-07 22:03:59 -0800

    Updates

--- IntegralEquation/Scripts-Horizontal-Well/p04_set_true_pert.py
+++ IntegralEquation/Scripts-Horizontal-Well/p04_set_true_pert.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     obj = ScatteringIntegralGeneralVzInversion2d(
         basedir=basedir,
         restart=True,
-        restart_code=None
+        restart_code=3
     )
     scale_fac_inv = obj.scale_fac_inv
 
