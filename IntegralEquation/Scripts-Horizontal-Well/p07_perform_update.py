First run after critical bug fix in update wavefield cg normal op calculation

IntegralEquation/Scripts-Horizontal-Well/p07_perform_update.py
@@ -20,74 +20,42 @@ if __name__ == "__main__":
     num_iter = int(sys.argv[1])
     num_procs = min(obj.num_sources, mp.cpu_count(), 100)
 
-    if num_iter == 0:
+    print("\n\n---------------------------------------------")
+    print("---------------------------------------------")
+    print("Perform update...")
+    print("\n")
 
-        print("\n\n---------------------------------------------")
-        print("---------------------------------------------")
-        print("Perform update...")
-        print("\n")
+    lambda_arr = np.zeros(shape=(obj.num_k_values, obj.num_sources), dtype=np.float32) + 1.0
+    mu_arr = np.zeros(shape=(obj.num_k_values, obj.num_sources), dtype=np.float32) + 1.0
 
-        lambda_arr = np.zeros(shape=(obj.num_k_values, obj.num_sources), dtype=np.float32) + 1.0
-        mu_arr = np.zeros(shape=(obj.num_k_values, obj.num_sources), dtype=np.float32) + 1.0
+    print("\n\n---------------------------------------------------------")
+    print("---------------------------------------------------------")
+    print("Iteration ", num_iter, ", updating wavefields...")
 
-        print("\n\n---------------------------------------------------------")
-        print("---------------------------------------------------------")
-        print("Iteration ", num_iter, ", updating wavefields...")
+    # Update wave field
+    obj.perform_inversion_update_wavefield(
+        iter_count=num_iter,
+        lambda_arr=lambda_arr,
+        mu_arr=mu_arr,
+        max_iter=40,
+        solver="cg",
+        atol=1e-5,
+        btol=1e-5,
+        num_procs=num_procs,
+        clean=True
+    )
 
-        # Update wave field
-        obj.perform_inversion_update_wavefield(
-            iter_count=num_iter,
-            lambda_arr=lambda_arr,
-            mu_arr=mu_arr,
-            max_iter=40,
-            solver="cg",
-            atol=1e-5,
-            btol=1e-5,
-            num_procs=num_procs,
-            clean=True
-        )
+    print("\n\n---------------------------------------------------------")
+    print("---------------------------------------------------------")
+    print("Iteration ", num_iter, ", updating perturbation...")
 
-        print("\n\n---------------------------------------------------------")
-        print("---------------------------------------------------------")
-        print("Iteration ", num_iter, ", updating perturbation...")
-
-        # Update pert
-        obj.perform_inversion_update_model_pert(
-            iter_count=num_iter,
-            max_iter=20,
-            tol=1e-5,
-            mnorm=0.001,
-            use_bounds=True,
-            num_procs=num_procs,
-            clean=True
-        )
-
-
-    if num_iter == 1:
-
-        print("\n\n---------------------------------------------")
-        print("---------------------------------------------")
-        print("Perform update...")
-        print("\n")
-
-        lambda_arr = np.zeros(shape=(obj.num_k_values, obj.num_sources), dtype=np.float32) + 1.0
-        mu_arr = np.zeros(shape=(obj.num_k_values, obj.num_sources), dtype=np.float32) + 0.0
-
-        print("\n\n---------------------------------------------------------")
-        print("---------------------------------------------------------")
-        print("Iteration ", num_iter, ", updating wavefields...")
-
-        # Update wave field
-        obj.perform_inversion_update_wavefield(
-            iter_count=num_iter,
-            lambda_arr=lambda_arr,
-            mu_arr=mu_arr,
-            max_iter=100,
-            solver="cg",
-            atol=1e-5,
-            btol=1e-5,
-            num_procs=num_procs,
-            clean=False
-        )
-
-        # TODO: We are testing iteration 1 with no mnorm and no bounds
+    # Update pert
+    obj.perform_inversion_update_model_pert(
+        iter_count=num_iter,
+        max_iter=20,
+        tol=1e-5,
+        mnorm=0.001,
+        use_bounds=True,
+        num_procs=num_procs,
+        clean=True
+    )