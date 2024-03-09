60bacb98d4400afd9971374df312d00dedf5cedd
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-08 16:08:17 -0800

    Debugging

--- IntegralEquation/Scripts-Horizontal-Well/BashScripts/p07.sh
+++ IntegralEquation/Scripts-Horizontal-Well/BashScripts/p07.sh
@@ -5,9 +5,9 @@ source .bashrc
 conda activate py39
 cd /home/rsarkar/Research/Thesis
 
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 0
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 0
 python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 1
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 2
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 3
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 4
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 5
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 2
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 3
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 4
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 5
