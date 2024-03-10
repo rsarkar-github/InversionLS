61cdc1a7c48994d5ba68de3685f368f6f0f0d8c8
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-09 20:02:36 -0800

    First run after critical bug fix in update wavefield cg normal op calculation

--- IntegralEquation/Scripts-Horizontal-Well/BashScripts/p07.sh
+++ IntegralEquation/Scripts-Horizontal-Well/BashScripts/p07.sh
@@ -5,13 +5,23 @@ source .bashrc
 conda activate py39
 cd /home/rsarkar/Research/Thesis
 
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 0
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 1
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 2
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 3
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 4
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 5
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 6
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 7
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 8
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 9
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 0
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 1
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 2
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 3
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 4
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 5
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 6
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 7
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 8
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 9
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 10
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 11
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 12
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 13
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 14
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 15
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 16
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 17
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 18
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 19
