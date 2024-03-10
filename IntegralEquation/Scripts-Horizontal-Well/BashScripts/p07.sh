b559bd2db316ff645e8601bbc0badc3b7b7d7dd9
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-10 12:54:19 -0700

    Updates

--- IntegralEquation/Scripts-Horizontal-Well/BashScripts/p07.sh
+++ IntegralEquation/Scripts-Horizontal-Well/BashScripts/p07.sh
@@ -5,5 +5,5 @@ source .bashrc
 conda activate py39
 cd /home/rsarkar/Research/Thesis
 
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 0 20
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 0 20
 python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p07_perform_update 1 20
