0b3c5576e15daac64a93530d2ce9819d70270c9d
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-13 16:36:04 -0700

    Horiz well Iter 9

--- IntegralEquation/Scripts-Horizontal-Well/BashScripts/p09.sh
+++ IntegralEquation/Scripts-Horizontal-Well/BashScripts/p09.sh
@@ -11,4 +11,5 @@ cd $SCRIPT_DIR
 cd ../../../..
 
 #python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p09_perform_update 7 40 0.000001
-python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p09_perform_update 8 40 0.000001
+#python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p09_perform_update 8 40 0.000001
+python -m InversionLS.IntegralEquation.Scripts-Horizontal-Well.p09_perform_update 9 40 0.000001
